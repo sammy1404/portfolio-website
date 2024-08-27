2b441c5d08125d4e1ac6fd45bb4edf2a58742eb7
sammy1404 <sammy1404>
2024-08-27 13:48:23 +0530

    hamburger menu works, elements to be added

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const hamMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
 hamMenu.addEventListener('click',()=>{
     hamMenu.classList.toggle('active')
+    menu.classList.toggle('active')
 })
+
+
+
