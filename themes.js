1943dcc593475b43daf0149b87f72e6fe0cc8b57
sammy1404 <sammy1404>
2024-08-28 22:56:10 +0530

    theme toggle made for desktop and mobile

---
+++ themes.js
@@ -0,0 +1,85 @@
+darkTheme = true
+
+
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+const themeButton = document.querySelector('.theme');
+
+
+
+const mobileThemeButton = document.querySelector('.menu .theme');
+const sunM = document.querySelector('.menu .sun');
+const moonM = document.querySelector('.menu .moon');
+
+
+themeButton.addEventListener('click',()=>{
+    darkTheme = !darkTheme;
+    console.log(darkTheme)
+
+
+    if (darkTheme) {
+        sun.classList.add('hide-sun');
+        sun.classList.remove('show-sun');
+        moon.classList.add('show-moon');
+        moon.classList.remove('hide-moon');
+
+
+        sunM.classList.add('hide-sun');
+        sunM.classList.remove('show-sun');
+        moonM.classList.add('show-moon');
+        moonM.classList.remove('hide-moon');
+    } else {
+        sun.classList.add('show-sun');
+        sun.classList.remove('hide-sun');
+        moon.classList.add('hide-moon');
+        moon.classList.remove('show-moon');
+
+
+        sunM.classList.add('show-sun');
+        sunM.classList.remove('hide-sun');
+        moonM.classList.add('hide-moon');
+        moonM.classList.remove('show-moon');
+    }
+});
+
+
+
+
+
+mobileThemeButton.addEventListener('click',()=>{
+    darkTheme = !darkTheme;
+    console.log(darkTheme)
+
+
+    if (darkTheme) {
+        sunM.classList.add('hide-sun');
+        sunM.classList.remove('show-sun');
+        moonM.classList.add('show-moon');
+        moonM.classList.remove('hide-moon');
+
+
+        sun.classList.add('hide-sun');
+        sun.classList.remove('show-sun');
+        moon.classList.add('show-moon');
+        moon.classList.remove('hide-moon');
+    } else {
+        sunM.classList.add('show-sun');
+        sunM.classList.remove('hide-sun');
+        moonM.classList.add('hide-moon');
+        moonM.classList.remove('show-moon');
+
+
+        sun.classList.add('show-sun');
+        sun.classList.remove('hide-sun');
+        moon.classList.add('hide-moon');
+        moon.classList.remove('show-moon');
+    }
+});
+
+if (darkTheme) {
+    sun.classList.add('hide-sun');
+    moon.classList.add('show-moon');
+} else {
+    sun.classList.add('show-sun');
+    moon.classList.add('hide-moon');
+}
